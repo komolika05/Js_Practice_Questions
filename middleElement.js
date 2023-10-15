21c636bca085b29b72b9c6bdb63a478afd4b0932
Komolika Kakrani <kanishkakakrani05@gmail.com>
2023-10-15 20:25:22 +0530

    Find/Identify the middle element if array is odd or find the middle -1 element if even

---
+++ middleElement.js
@@ -0,0 +1,17 @@
+//Find the middle element if array is odd
+
+function middleElement(array){
+
+    if(array.length%2 === 0){
+    let middleValue = (array.length-2)/2
+
+    return array[middleValue]
+    }
+    else {
+        let middleValue = (array.length-1)/2
+
+    return array[middleValue]
+    }
+}
+const array = middleElement([1,2,3,4,5,6])
+console.log(array)
