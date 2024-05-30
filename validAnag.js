eb989cba4bb2780b211c9f7b8c079a636da83017
Komolika Kakrani <kanishkakakrani05@gmail.com>
2024-05-30 13:06:04 +0530

    improve time complexity for validAnag

--- validAnag.js
+++ validAnag.js
@@ -1,7 +1,8 @@
 //Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 function validAnagram(s, t) {
-  let count = {};
+  let count1 = {};
+  let count2 = {};
 
   if (s.length !== t.length) {
     return false;
@@ -10,29 +11,40 @@ function validAnagram(s, t) {
   for (let i = 0; i < s.length; i++) {
     const element1 = s[i];
 
-    if (count[element1]) {
-      count[element1] += 1;
+    if (count1[element1]) {
+      count1[element1] += 1;
     } else {
-      count[element1] = 1;
+      count1[element1] = 1;
     }
   }
 
   for (let i = 0; i < t.length; i++) {
     const element2 = t[i];
 
-    if (!count[element2]) {
-      return false;
+    if (count2[element2]) {
+      count2[element2] += 1;
     } else {
-      count[element2] -= 1;
+      count2[element2] = 1;
+    }
+  }
+  return compareObjects(count1, count2);
+}
+
+function compareObjects(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
     }
   }
 
-  for (let key in count) {
-    if (count[key] !== 0) {
-      return false;
-    }
-  }
   return true;
 }
 
-console.log(validAnagram("anagram", "nagaram"));
+console.log(validAnagram("rat", "car"));
