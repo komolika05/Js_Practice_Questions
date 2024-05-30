dad2c26a0f6f9d5b0fe98757aa4506127c3a1b71
Komolika Kakrani <kanishkakakrani05@gmail.com>
2024-05-30 10:37:42 +0530

    check if s and t are valid anagram or not

--- validAnag.js
+++ validAnag.js
@@ -1,8 +1,6 @@
 //Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
 function validAnagram(s, t) {
-  const arr1 = s.split("");
-  const arr2 = t.split("");
   let count = {};
 
   if (s.length !== t.length) {
@@ -10,9 +8,31 @@ function validAnagram(s, t) {
   }
 
   for (let i = 0; i < s.length; i++) {
-    const element1 = arr1[i];
+    const element1 = s[i];
+
+    if (count[element1]) {
+      count[element1] += 1;
+    } else {
+      count[element1] = 1;
+    }
   }
-  return false;
+
+  for (let i = 0; i < t.length; i++) {
+    const element2 = t[i];
+
+    if (!count[element2]) {
+      return false;
+    } else {
+      count[element2] -= 1;
+    }
+  }
+
+  for (let key in count) {
+    if (count[key] !== 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
-console.log(validAnagram("anagram", "nagram"));
+console.log(validAnagram("anagram", "nagaram"));
